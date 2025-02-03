096d7e6d50d952d026fdb6251029c6738c6e51d0
Purplan <noah.j.groth@gmail.com>
2025-02-03 20:34:09 +0100

    Revert ""
    This reverts commit ea7fd8e7f463cb040333255ce9b390c375bd5a31.

--- src/main/java/net/mcreator/minecrafttheforgottenworld/procedures/SleepingPodGUIWhileThisGUIIsOpenTickProcedure.java
+++ src/main/java/net/mcreator/minecrafttheforgottenworld/procedures/SleepingPodGUIWhileThisGUIIsOpenTickProcedure.java
@@ -1,24 +1,12 @@
 package net.mcreator.minecrafttheforgottenworld.procedures;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.client.gui.components.Checkbox;
-
-import net.mcreator.minecrafttheforgottenworld.network.MinecraftTheForgottenWorldModVariables;
-
-import java.util.HashMap;
+import net.minecraftforge.eventbus.api.Event;
 
 public class SleepingPodGUIWhileThisGUIIsOpenTickProcedure {
-	public static void execute(Entity entity, HashMap guistate) {
-		if (entity == null || guistate == null)
+	public static void execute(HashMap guistate) {
+		if (guistate == null)
 			return;
 		if (guistate.containsKey("checkbox:SleepingPodRadioactiveProtection") && ((Checkbox) guistate.get("checkbox:SleepingPodRadioactiveProtection")).selected()) {
-			{
-				boolean _setval = true;
-				entity.getCapability(MinecraftTheForgottenWorldModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-					capability.is_wearing_hazmat_suit = _setval;
-					capability.syncPlayerVariables(entity);
-				});
-			}
 		}
 	}
 }
