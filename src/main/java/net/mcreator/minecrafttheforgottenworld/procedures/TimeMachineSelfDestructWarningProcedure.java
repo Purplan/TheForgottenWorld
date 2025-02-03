d4e525c7bfe57813e0fe46669b37d8400b745fa3
Tomas Viberg <Tomas.Viberg@kambi.com>
2025-02-03 19:07:08 +0100

    MCreator Merge commit 'c6c9125ff01f15d745703d2af998a59b10ecbdff' into d46b0f433c6d92d1c616c3d9e79f44450fb05583

--- src/main/java/net/mcreator/minecrafttheforgottenworld/procedures/TimeMachineSelfDestructWarningProcedure.java
+++ src/main/java/net/mcreator/minecrafttheforgottenworld/procedures/TimeMachineSelfDestructWarningProcedure.java
@@ -1,5 +1,7 @@
 package net.mcreator.minecrafttheforgottenworld.procedures;
 
+import net.minecraftforge.eventbus.api.Event;
+
 public class TimeMachineSelfDestructWarningProcedure {
 	public static void execute() {
 	}
